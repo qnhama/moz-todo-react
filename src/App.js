7d279343697239c5be6e62a8959a331180e117f9
qnhama <qnhama@gmail.com>
2021-07-12 17:48:26 -0500

    Unique keys

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import Todo from "./components/Todo";
 
 function App(props) {
 
-  const taskList = props.tasks.map(task => <Todo />);
-   
+  const taskList = props.tasks.map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}  //key is a special prop that's managed by React – you cannot use the word key for any other purpose. Because keys should be unique, we're going to re-use the id of each task object as its key. Update your taskList constant like so:
+    />
+  )
+); //Rendering with iteration
+
   console.log(props);
   const subject = props.subject;
   return (
