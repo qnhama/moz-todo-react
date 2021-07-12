2e2f0fdfa1ac3749668aaf6cfc2f404101282ddb
qnhama <qnhama@gmail.com>
2021-07-12 17:56:46 -0500

    Componentizing the rest of the app

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const DATA = [
   { id: "todo-0", name: "Eat", completed: true },
   { id: "todo-1", name: "Sleep", completed: false },
   { id: "todo-2", name: "Repeat", completed: false }
-];
+]; //Rendering with iteration
 
 ReactDOM.render(
   <React.StrictMode>
