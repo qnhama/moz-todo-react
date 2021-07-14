8673cdce751cd73267e03867cdc5d9d4eff817a6
qnhama <qnhama@gmail.com>
2021-07-14 18:12:38 -0500

    Reading user input

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,18 @@ import React, { useState } from "react";
 
 function Form(props) {
 
-  const [name, setName] = useState('Use hooks!');
+  const [name, setName] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
     props.addTask("Say hello! since prp");
   }
+
+  
+  function handleChange(e) {
+    console.log(e.target.value);
+  }
+
 
   return (
     <form onSubmit={handleSubmit}>
@@ -25,12 +31,15 @@ function Form(props) {
         name="text"
         autoComplete="off"
         value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
       </button>
     </form>
   );
+
+  
 }
 
 export default Form;
