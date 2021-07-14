145bf6375ea446c1b0ca023415432da04f536a4e
qnhama <qnhama@gmail.com>
2021-07-14 18:06:19 -0500

    Reading state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,9 @@
 
-import React from "react";
-
-
+import React, { useState } from "react";
 
 function Form(props) {
+
+  const [name, setName] = useState('Use hooks!');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -24,6 +24,7 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
