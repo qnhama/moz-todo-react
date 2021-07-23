c3508305465d39996b28dd606bd01156b669e3a8
Mauricio Yeudiel Cunjama Torres <qnhama@gmail.com>
2021-07-22 22:59:28 -0500

    Prepare the heading

--- src/App.js
+++ src/App.js
@@ -92,8 +92,9 @@ const taskList = tasks
       <div className="filters btn-group stack-exception">
         {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
-      <ul
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>      <ul
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
